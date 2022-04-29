a41f2d3ef6fb4cd8bc44d8425d328b713f474eba
itcnto24 <104519665+itcnto24@users.noreply.github.com>
2022-04-29 13:41:51 -0500

    Archivos Ciclo For
    Aprendimos sobre el ciclo For y luego juntamos arrays con ciclos (while, do/whiley for)

---
+++ Ciclos II - Italia/array-bucles.js
@@ -0,0 +1,36 @@
+// Declaramos un Array
+
+var pokemones=["Chikorita", "Balbasour", "Charizar"];
+
+// Definir nuestra variable i
+
+var i=0;
+
+// Ciclo While
+/* 
+while(i<=2){
+    // Código que se ejecuta
+    console.log(pokemones[i]);
+    i++;
+} 
+*/
+
+// Ciclo Do/While
+
+/* 
+do{
+//Código a 
+console.log(pokemones[i]);
+i++;
+} while(i<=2); 
+*/
+
+// Ciclo For
+
+for(var i=0; i<=2; i++){
+    // Código que se ejecuta
+// console.log(pokemones[i]);
+document.write(pokemones[i] + " yo te elijo! <br>");
+}
+
+
---
+++ Ciclos II - Italia/script.js
@@ -0,0 +1,32 @@
+/* 
+
+Bucle For
+
+Determinado
+
+Su sintaxis consta de 3 partes:
+     - Variable
+     - Número de interacciones
+     - Incrementación o Decrementación
+    Seperadas por ";"
+
+for (variable; numero-iteraciones; incrementación/decrementación){
+      Código se ejecuta
+}
+*/
+
+
+// Ciclo For incremental
+/* 
+
+for(var i=0; i<=10; i++){
+console.log(i);
+} 
+
+*/
+
+// Ciclo For Decremental
+
+for(var i=10; i>0; i--){
+console.log(i);
+}
